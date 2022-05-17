a225756eccaebe9493b0bd3549129aeb526f570e
Seligmann <zachary.a.seligman@gmail.com>
2022-05-17 01:17:00 -0500

    really basic and shitty placeholder client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import burself from './images/burself.png';
 const App = () => {
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">frfr ong</Typography>
+                <img src={burself} alt="emotes" height="60"/>
+            </AppBar>
+
+        </Container>
     )
 }
 
