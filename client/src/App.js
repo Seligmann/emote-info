e24612a55511260505938da03f3e0a1499c23b7b
Seligmann <zachary.a.seligman@gmail.com>
2022-05-17 16:02:37 -0500

    style for client

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Dggers from "./components/Dggers/Dggers";
 import Form from "./components/Form/Form";
 import burself from './images/burself.png';
+import useStyles from './styles';
 
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">frfr ong</Typography>
-                <img src={burself} alt="emotes" height="60"/>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">frfr ong</Typography>
+                <img className={classes.image} src={burself} alt="emotes" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
