703c408266ed606b569d66696ac5a5fea7e9b56f
Seligmann <zachary.a.seligman@gmail.com>
2022-05-25 15:53:23 -0500

    fixed issue of the server crashing after a single post was made

--- server/controllers/dggers.js
+++ server/controllers/dggers.js
@@ -4,9 +4,9 @@ import DggerEmoteList from "../models/dggerEmoteList.js";
 export const getDggers = async (req, res) => {
     try {
         const dggerEmotes = await DggerEmoteList.find();
-        res.status(200).json(dggerEmotes);
+        return res.status(200).json(dggerEmotes);
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        return res.status(404).json({ message: error.message });
     }
 }
 
@@ -17,9 +17,8 @@ export const createDgger = async (req, res) => {
     try {
         // console.log(dgger.username);
         await newDgger.save();
-        res.status(201).json(newDgger);
+        return res.status(201).json(newDgger);
     } catch (error) {
-        res.status(409).json({ message: error.message() });
+        return res.status(409).json({ message: error.message() });
     }
-    res.send('Dgger Creation');
 }
