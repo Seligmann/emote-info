2e88f59a12135fa0fd3d15a48afdb0701b1a9af9
Seligmann <zachary.a.seligman@gmail.com>
2022-06-08 21:53:54 +0000

    changed to www host temporarily

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://zelig.dev/dggers';
+const url = 'https://www.zelig.dev/dggers';
 
 export const fetchDggers = () => axios.get(url);
 export const createDgger = (newDgger) => axios.post(url, newDgger);
