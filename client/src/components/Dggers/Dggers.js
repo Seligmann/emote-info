0433d19240a6d6058a4f63abe9b9f7ddf1f992ec
Seligmann <zachary.a.seligman@gmail.com>
2022-07-02 16:47:10 -0500

    added component to show all users' emote usage

--- client/src/components/Dggers/Dggers.js
+++ client/src/components/Dggers/Dggers.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
-import Dgger from './Dgger/Dgger';
-import useStyles from './styles';
+// import Dgger from './Dgger/Dgger';
+import { Dgger } from './Dgger/Dgger';
+// import useStyles from './styles';
 
-const Dggers = () => {
-    const dggers = useSelector((state) => state.dggers);
-    const classes = useStyles();
-
-    console.log(dggers);
+export const Dggers = (props) => {
+    if (props.loading) return <p>Emote use info is being loaded...</p>
 
     return (
-        <>
-            <h1>DGGERS</h1>
-            <Dgger/>
-            <Dgger/>
-        </>
-    );
-}
+    <table className="table">
+        <thead>
+          <tr>
+            <th className="table-head-item" />
 
-export default Dggers;
+            <th className="table-head-item">Username</th>
+
+            <th className="table-head-item">Emotes</th>
+
+            <th className="table-head-item">Count</th>
+
+            <th className="table-head-item" />
+          </tr>
+        </thead>
+
+        <tbody className='table-body'>
+            {props.dggers.length > 0 ? (
+                props.dggers.map((dgger) => (
+                    <Dgger
+                        username={dgger.username}
+                        emotes={dgger.emotes}
+                        uses={dgger.uses}
+                    />
+                    )
+                )
+            ) : (
+                <tr className='table-row'>
+                    <td className='table-item' style={{textAlign: 'center'}} colSpan={6}>No users to show.</td>
+                </tr>
+            )
+            }
+        </tbody>
+    </table>
+
+    )
+}
