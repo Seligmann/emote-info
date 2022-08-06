ad3fd9aa57c9d9f5f7458acf4c25159938cb2570
Seligmann <zachary.a.seligman@gmail.com>
2022-08-05 22:37:03 -0500

    made disgusting strings look less disgusting (although still very hardcode-y :)

--- server/controllers/dggers.js
+++ server/controllers/dggers.js
@@ -1,6 +1,6 @@
 // import {updateDggerTable, getEmoteList} from "../models/dggerEmoteList.js";
 import axios from "axios";
-import Database from 'better-sqlite3';
+import Database from "better-sqlite3";
 
 async function allMonthsYears() {
   try {
@@ -21,31 +21,13 @@ async function userLogUrls(allMonthsYears, username) {
       const month = allMonthsYears[i].split(" ")[0].trim();
       const year = allMonthsYears[i].split(" ")[1].trim();
 
-      const response = await axios.get(
-        "https://overrustlelogs.net/api/v1/Destinygg" +
-          "/" +
-          month +
-          "%20" +
-          year +
-          "/users.json"
-      );
+      const response = await axios.get(`https://overrustlelogs.net/api/v1/Destinygg/${month} ${year}/users.json`);
 
       // Check if the desired user was active in specific month and year
       response.data.forEach((tmp) => {
         const activeUsername = tmp.substring(0, tmp.length - 4);
         if (username.toLowerCase() === activeUsername.trim().toLowerCase()) {
-          userMonthYearUrls.push(
-            "https://overrustlelogs.net/Destinygg%20chatlog" +
-              "/" +
-              month +
-              "%20" +
-              year +
-              "/" +
-              "userlogs" +
-              "/" +
-              username +
-              ".txt"
-          );
+          userMonthYearUrls.push(`https://overrustlelogs.net/Destinygg%20chatlog/${month} ${year}/userlogs/${username}.txt`);
         }
       });
     } catch (error) {
@@ -61,26 +43,28 @@ async function userEmoteUsage(userMonthYearUrls, username) {
   let emotes = [];
 
   // Get current list of active emotes on dgg
-  const response = await axios.get('https://cdn.destiny.gg/emotes/emotes.json');
-  response.data.map(emoteInfo => {emotes.push(emoteInfo.prefix)});
-   
+  const response = await axios.get("https://cdn.destiny.gg/emotes/emotes.json");
+  response.data.map((emoteInfo) => {
+    emotes.push(emoteInfo.prefix);
+  });
+
   // Get emote usage
   for (let i = 0; i < userMonthYearUrls.length; i++) {
     try {
       const response = await axios.get(userMonthYearUrls[i]);
       const messages = response.data.split(/\n/);
-      
+
       messages.forEach((tmp) => {
         const message = tmp.split(/[, ]+/);
 
-        emotes.forEach(emote => {
+        emotes.forEach((emote) => {
           for (const word of message) {
             if (word === emote) {
               updates[emote] = isNaN(updates[emote]) ? 1 : updates[emote] + 1;
               break;
             }
           }
-        })
+        });
       });
     } catch (error) {
       console.log(error);
@@ -88,19 +72,18 @@ async function userEmoteUsage(userMonthYearUrls, username) {
   }
 
   // Update db
-  const db = new Database('dggers.db', {verbose:console.log});
+  const db = new Database("dggers.db", { verbose: console.log });
   for (let emote in updates) {
-    const stmt = db.prepare('INSERT INTO emote_info VALUES (?, ?, ?)');
+    const stmt = db.prepare("INSERT INTO emote_info VALUES (?, ?, ?)");
     stmt.run(username, emote, updates[emote]);
-
   }
 }
 
 // Controllers
 export const getDggers = async (req, res) => {
   try {
-    const db = new Database('dggers.db', {verbose : console.log});
-    const dggerEmotes = db.prepare('SELECT * FROM emote_info').all();
+    const db = new Database("dggers.db", { verbose: console.log });
+    const dggerEmotes = db.prepare("SELECT * FROM emote_info").all();
     return res.status(200).json(dggerEmotes);
   } catch (error) {
     return res.status(404).json({ message: error.message });
