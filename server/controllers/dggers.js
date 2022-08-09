db60ac77c37c7f91244270008451e90e004bb52c
Seligmann <zachary.a.seligman@gmail.com>
2022-08-09 16:10:24 -0500

    update user route partially added (insertion in DB and creating actual route to-be-done)

--- server/controllers/dggers.js
+++ server/controllers/dggers.js
@@ -79,6 +79,72 @@ async function userEmoteUsage(userMonthYearUrls, username) {
 }
 
 // Controllers
+export const updateLogs = async (req, res) => {
+  try {
+    let date = req.query.date; // NOTE there might be an issue with conditionals regarding dates being compared if one thing is an int
+    // one thing is all lowercase, one thing has one capital letter starting it off, etc... (i dont think this is
+    // the case but if something goes terribly wrong.. check)
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1; // months are 0-indexed
+    let day = date.getDate() - 1; // logs in ORL are stored the day after
+    if (day === 0) {
+      if (month === 1) {
+        year -= 1;
+      }
+
+      month -= 1;
+      day = [1, 3, 5, 7, 8, 10, 12].includes(month) ? 31 : 30;
+    }
+
+    const db = new Database("logs.db", { verbose: console.log });
+    const recentYear = db.prepare("SELECT MAX(year) FROM logs").run();
+    const recentMonth = db
+      .prepare("SELECT MAX(month) FROM logs WHERE year=(?)")
+      .run(recentYear);
+    const recentDay = db
+      .prepare("SELECT MAX(day) FROM logs WHERE year=(?) AND month=(?)")
+      .run(recentYear, recentMonth);
+
+    // is it even possible to hardcode this any more for dgg ... -_-
+    // This is only useful if logs from twitch also have the date for each message (which I would think they would...)
+    while (
+      !(year === recentYear && month === recentMonth && day === recentDay)
+    ) {
+
+      // add messages to db
+      const response = await axios.get(
+        `https://overrustlelogs.net/Destinygg%20chatlog/${month} ${year}/${day}.txt`
+      );
+
+      const messages = response.data.split(/\n/);
+      messages.forEach((message) => {
+        // const message = tmp.split(/[, ]+/);
+
+        // TODO: insert each chat message into db // db.prepare("INSERT INTO logs VALUES (?, ?, ")
+      });
+
+      // change year, month, day
+      if (day === 1) {
+        month -= 1;
+
+        if (month === 0) {
+          year -= 1;
+
+          if (year === 2012) {
+            break;
+          }
+
+          month = 12;
+        }
+
+        day = [1, 3, 5, 7, 8, 10, 12].includes(month) ? 31 : 30;
+      }
+    }
+  } catch (error) {
+    console.log(`error while updating logs database: ${error}`);
+  }
+};
+
 export const removeUser = async (req, res) => {
 	try {
 		let username = req.query.username;
