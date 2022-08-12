9af5905166122a93ca5a838aa4e149823791236b
Seligmann <zachary.a.seligman@gmail.com>
2022-08-11 21:32:45 -0500

    added emote images POGGIES ... tables still need 2 be styled to not look like shit

--- client/src/components/Dggers/Dggers.js
+++ client/src/components/Dggers/Dggers.js
@@ -2,39 +2,40 @@ import React from "react";
 
 
 export const Dggers = (props) => (
-    <table className="table">
-      <thead>
-        <tr>
-          <th className="table-head-item">Emotes</th>
+  <table className="table">
+    <thead>
+      <tr>
+        <th className="table-head-item">Emotes</th>
 
-          <th className="table-head-item">Count</th>
-        </tr>
-      </thead>
+        <th className="table-head-item">Count</th>
+      </tr>
+    </thead>
 
-      <tbody className="table-body" style={{ textAlign: "center" }}>
-        {props.users?.length > 0 ? (
-          Object.keys(props.users).map((key, i) => (
-			<tr className="table-row">
-    		    <td className="table-item">
-    		        {props.users[key].emote}
-    		    </td>
+    <tbody className="table-body" style={{ textAlign: "center" }}>
+      {props.users?.length > 0 ? (
+        Object.keys(props.users).map((key, i) => (
+          <tr className="table-row">
+            <td className="table-item"><img src={props.users[key].emote_image}></img></td>
+            <td className="table-item">{props.users[key].emote}</td>
+            <td className="table-item">{props.users[key].uses}</td>
 
     		    <td className="table-item">
     		        {props.users[key].uses}
     		    </td>
-    		</tr>
-          ))
-        ) : (
-          <tr className="table-row">
-            <td
-              className="table-item"
-              style={{ textAlign: "center" }}
-              colSpan={6}
-            >
-				Please search for a user, or wait for the profile searched to load...
-            </td>
           </tr>
-        )}
-      </tbody>
-    </table>
-  );
+        ))
+      ) : (
+        <tr className="table-row">
+          <td
+            className="table-item"
+            style={{ textAlign: "center" }}
+            colSpan={6}
+          >
+            Please search for a user, or wait for the profile searched to
+            load...
+          </td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
