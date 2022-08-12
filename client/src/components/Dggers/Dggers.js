d7daf52e6acf4b38b059d5d0151274c444dbca16
Seligmann <zachary.a.seligman@gmail.com>
2022-08-11 22:54:48 -0500

    table aesthetic improved

--- client/src/components/Dggers/Dggers.js
+++ client/src/components/Dggers/Dggers.js
@@ -1,36 +1,50 @@
 import React from "react";
 
-
 export const Dggers = (props) => (
-  <table className="table">
+  <table style={{width: "300px"}}>
+
+    <style>{`
+    table {
+      border-collapse: collapse;
+      margin-left: auto;
+      margin-right: auto;
+    }
+    thead {
+      background-color: #333;
+      font-size: 0.875rem;
+      text-transform: uppercase;
+      letter-spacing: 2%;
+    }
+    tbody tr:nth-child(odd) {
+      background-color: #505050;
+    }
+    
+    tbody tr:nth-child(even) {
+      background-color: #5c5c5c;
+    }
+    `}</style> 
+
     <thead>
       <tr>
-        <th className="table-head-item">Emotes</th>
-
-        <th className="table-head-item">Count</th>
+        <th>Emotes</th>
+        <th>Count</th>
       </tr>
     </thead>
 
-    <tbody className="table-body" style={{ textAlign: "center" }}>
+    <tbody style={{ textAlign: "center" }}>
       {props.users?.length > 0 ? (
         Object.keys(props.users).map((key, i) => (
-          <tr className="table-row">
-            <td className="table-item"><img src={props.users[key].emote_image}></img></td>
-            <td className="table-item">{props.users[key].emote}</td>
-            <td className="table-item">{props.users[key].uses}</td>
-
-    		    <td className="table-item">
-    		        {props.users[key].uses}
-    		    </td>
+          <tr>
+            <td>
+              <img src={props.users[key].emote_image}></img>
+            </td>
+            <td>{props.users[key].emote}</td>
+            <td>{props.users[key].uses}</td>
           </tr>
         ))
       ) : (
-        <tr className="table-row">
-          <td
-            className="table-item"
-            style={{ textAlign: "center" }}
-            colSpan={6}
-          >
+        <tr>
+          <td style={{ textAlign: "center" }} colSpan={6}>
             Please search for a user, or wait for the profile searched to
             load...
           </td>
