c6f0b6d17fe3c6a43c32e439d6fd9affe2acf119
Seligmann <zachary.a.seligman@gmail.com>
2022-08-24 19:17:04 -0500

    fixed example that was supposed to have code tags

--- client/src/App.js
+++ client/src/App.js
@@ -243,7 +243,7 @@ const App = () => {
                     twitch.tv/xqc
                   </a>{" "}
                   in the future, and possibly more channels. Simply search for a
-                  username (e.g. `ze1ig`, `cake`) to get started.
+                  username (e.g. <code>ze1ig</code>, <code>cake</code>) to get started.
                 </p>
               </div>
             </Grid>
